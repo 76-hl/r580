c60ce2943933995fbea53e0aa51ac9afe651e2f8
76-hl <17379089717@163.com>
2025-03-18 09:40:17 +0800

    Delete codec directory

--- codec/OBJ/nau8814.d
+++
@@ -1,4 +0,0 @@
-..\obj\nau8814.o: ..\HARDWARE\codec_driver\nau8814.c
-..\obj\nau8814.o: ..\HARDWARE\codec_driver\nau8814.h
-..\obj\nau8814.o: D:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-..\obj\nau8814.o: ..\HARDWARE\codec_driver\reg.h
